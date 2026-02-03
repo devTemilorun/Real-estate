4fdf9a1be9f47c069b91d142f3f351980436266d
Temilorun Olawuni <dev.temilorun@gmail.com>
2026-02-03 22:45:34 +0100

    added the Testimonials Page

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets, testimonialsData } from '../assets/assets'
+
+const Testimonials = () => {
+  return (
+    <div id='Testimonials' className='container mx-auto py-10 lg:px-32 w-full overflow-hidden'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
+      <p className='text-gray-500 mx-auto max-w-80 text-center mb-12'>Real Stories from Those Who Found Home with Us</p>
+      
+
+      <div className='flex flex-wrap justify-center gap-8'>
+        {testimonialsData.map((testimonial, index)=>(
+            <div key={index} className='max-w-[300px] border border-gray-200 shadow-lg rounded px-8 py-12 text-center'>
+                <img src={testimonial.image} alt={testimonial.alt} className='w-20 h-20 rounded-full mx-auto mb-4' />
+                <h2 className='text-xl text-gray-700 font-medium'>{ testimonial.name }</h2>
+                <p className='mb-4 text-gray-500'>{ testimonial.title }</p>
+                <div className='flex justify-center gap-1 text-red-500 mb-4'>
+                    {Array.from({length: testimonial.rating}, (item, index)=>(
+                        <img key={index} src={assets.star_icon} alt="" />
+                    ))}
+                </div>
+                <p className='text-gray-600'>{ testimonial.text }</p>
+            </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
